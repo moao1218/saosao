e20ebb18363d5663363846bde8c6f25a11a86b82
shanxnang <shanxnang@gmail.com>
2019-05-14 20:05:31 +0800

    ;;;;;

--- src/main/java/cn/saosao/controller/PendingController.java
+++ src/main/java/cn/saosao/controller/PendingController.java
@@ -1,6 +1,9 @@
 package cn.saosao.controller;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,9 +32,12 @@ public class PendingController {
 	@Autowired
 	IStatusService iStatusService;
 	@RequestMapping("/pending")
-	public String pending(Model model,Claim_List claim,String cp,String start_time,String end_time,String timegap,HttpServletRequest request) {
+	public String pending(Model model,Claim_List claim,String cp,String start_time,String end_time,String timegap,HttpServletRequest request) throws ParseException {
 		HttpSession session = request.getSession();
 		Clerk getClerk=(Clerk)session.getAttribute("clerk");
+		
+		SimpleDateFormat sim=new SimpleDateFormat("yyyy-MM-dd");
+		Date date=new Date();
 		
 		model.addAttribute("claim",claim);
 		model.addAttribute("start_time",start_time);
@@ -70,6 +76,22 @@ public class PendingController {
 		iClaimListService.getAll(map);
 		List<Status> allStatus = iStatusService.findAllStatus();
 		List<Claim_List> lista = (List)map.get("claim_list");
+		
+		for (Claim_List claim_List : lista) {
+			String claim_date = claim_List.getUpper_date();
+			Date parse = sim.parse(claim_date);
+			Long emergencytime=(date.getTime()-parse.getTime())/(1000*3600*24);
+			if(emergencytime<=50) {
+				claim_List.setEmergency("正常");
+			}else if(emergencytime<=80){
+				claim_List.setEmergency("紧急");
+			}else {
+				claim_List.setEmergency("非常紧急");
+			}
+			System.out.println(emergencytime);
+			
+		}
+		
 		BigDecimal line_count=(BigDecimal) map.get("v_count");
 		BigDecimal totlepage=(BigDecimal)map.get("totlepage");
 		model.addAttribute("cp",ccp);
