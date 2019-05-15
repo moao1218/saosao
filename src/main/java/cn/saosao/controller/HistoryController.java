59a3403c76bce202d80489655c41564d13bfcade
shanxnang <shanxnang@gmail.com>
2019-05-15 09:23:55 +0800

    ll

--- src/main/java/cn/saosao/controller/HistoryController.java
+++ src/main/java/cn/saosao/controller/HistoryController.java
@@ -1,6 +1,9 @@
 package cn.saosao.controller;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,12 +31,11 @@ public class HistoryController {
 	@Autowired
 	IStatusService iStatusService;
 	@RequestMapping("/history")
-	public String getHistory(Model model,Claim_List claim,String cp,String start_time,String end_time,String timegap,HttpServletRequest request) {
+	public String getHistory(Model model,Claim_List claim,String cp,String start_time,String end_time,String timegap,HttpServletRequest request) throws ParseException {
 		HttpSession session = request.getSession();
 		Clerk getClerk=(Clerk)session.getAttribute("clerk");
-		System.out.println(getClerk);
-		System.out.println(start_time);
-		System.out.println(end_time);
+		SimpleDateFormat sim=new SimpleDateFormat("yyyy-MM-dd");
+		Date date=new Date();
 		model.addAttribute("claim",claim);
 		model.addAttribute("start_time",start_time);
 		model.addAttribute("end_time",end_time);
@@ -73,6 +75,21 @@ public class HistoryController {
 		iClaimListService.getAll(map);
 		List<Status> allStatus = iStatusService.findAllStatus();
 		List<Claim_List> lista = (List)map.get("claim_list");
+		
+		for (Claim_List claim_List : lista) {
+			String claim_date = claim_List.getUpper_date();
+			Date parse = sim.parse(claim_date);
+			Long emergencytime=(date.getTime()-parse.getTime())/(1000*3600*24);
+			if(emergencytime<=50) {
+				claim_List.setEmergency("正常");
+			}else if(emergencytime<=80){
+				claim_List.setEmergency("紧急");
+			}else {
+				claim_List.setEmergency("非常紧急");
+			}
+			
+		}
+		
 		BigDecimal line_count=(BigDecimal) map.get("v_count");
 		BigDecimal totlepage=(BigDecimal)map.get("totlepage");
 		System.out.println(line_count);
